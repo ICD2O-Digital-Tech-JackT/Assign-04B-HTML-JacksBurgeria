5fc9500a9061f482ee5d47ff7ee2505c009d6053
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-04 00:51:34 +0000

    finished rest of visuals, did toppings code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+let PicklesN = 0;
+let TomatoesN = 0;
+let MeatN = 1;
+
+
+function UpdateVisuals(P,T,M){
+  document.getElementById('pickleDis').innerHTML = "Pickles"+"("+P+")";
+  document.getElementById('tomatoDis').innerHTML = "Tomatoes"+"("+T+")";
+  document.getElementById('meatDis').innerHTML = "Meat"+"("+M+")";
+}
+
+function addTop(Type){
+  if (Type == "Pickles"){
+    PicklesN=PicklesN+1;
+  } else if (Type == "Tomatoes"){
+    TomatoesN=TomatoesN+1;
+  } else if (Type == "Meat"){
+    MeatN=MeatN+1;
+  }
+  UpdateVisuals(PicklesN,TomatoesN,MeatN)
+}
+function delTop(Type){
+  if (Type == "Pickles"){
+    PicklesN=PicklesN-1;
+  } else if (Type == "Tomatoes"){
+    TomatoesN=TomatoesN-1;
+  } else if (Type == "Meat"){
+    MeatN=MeatN-1;
+  }
+  UpdateVisuals(PicklesN,TomatoesN,MeatN)
+}
+function DisplayCurrentOrder(){
+  
+}
+function FinishOrder(){
+  
+}
