94bf963a18ff1222a1b39a3971834e6654aba4a8
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-06 21:01:03 +0000

    Confirmed site is accurate, final edit

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let CurrentOrderPrice = 0
 const HST = 0.13;
 
 function ResetText() {
-  document.getElementById("finish order").innerHTML="Finish Order"
+  document.getElementById("finish order").innerHTML = "Finish Order"
 }
 
 function UpdateVisuals() {
@@ -17,16 +17,16 @@ function UpdateVisuals() {
   document.getElementById('pickleDis').innerHTML = "Pickles" + "(" + PicklesN + ")";
   document.getElementById('tomatoDis').innerHTML = "Tomatoes" + "(" + TomatoesN + ")";
   document.getElementById('meatDis').innerHTML = "Meat" + "(" + MeatN + ")";
-  if (PicklesN!=0||TomatoesN!=0||MeatN!=1){
+  if (PicklesN != 0 || TomatoesN != 0 || MeatN != 1) {
     document.getElementById("BurgProg").style.backgroundColor = "green";
-  }else if(Size!="medium"){
+  } else if (Size != "medium") {
     document.getElementById("BurgProg").style.backgroundColor = "green";
-  } else{
+  } else {
     document.getElementById("BurgProg").style.backgroundColor = "gray";
   }
-  if(Drink.value!="none"){
+  if (Drink.value != "none") {
     document.getElementById("DrinkProg").style.backgroundColor = "green";
-  } else{
+  } else {
     document.getElementById("DrinkProg").style.backgroundColor = "gray";
   }
 }
@@ -63,32 +63,33 @@ function delTop(Type) {
   }
   UpdateVisuals()
 }
+
 function CalculatePrice() {
   Drink = document.getElementById("drink");
   Size = document.getElementById("sizeselect").value;
   CurrentOrderPrice = 0
   if (Size == "medium") {
-    CurrentOrderPrice +=2.99
+    CurrentOrderPrice += 2.99
   } else if (Size == "large") {
-    CurrentOrderPrice +=3.99
+    CurrentOrderPrice += 3.99
   } else if (Size == "jumbotron") {
-    CurrentOrderPrice +=7.99
+    CurrentOrderPrice += 7.99
   }
   CurrentOrderPrice += (PicklesN * 0.25)
   CurrentOrderPrice += (TomatoesN * 0.25)
-  CurrentOrderPrice += ((MeatN-1) * 0.85)/*Minus one because only extra meat adds to the price*/
-  
-  if (Drink.value!="notchosen"){
-    CurrentOrderPrice+=3.50
+  CurrentOrderPrice += ((MeatN - 1) * 0.85)/*Minus one because only extra meat adds to the price*/
+
+  if (Drink.value != "notchosen") {
+    CurrentOrderPrice += 3.50
   }
-  CurrentOrderPrice*=1.13 /*Tax*/
-  CurrentOrderPrice = Math.round(CurrentOrderPrice*100)/100
+  CurrentOrderPrice *= 1.13 /*Tax*/
+  CurrentOrderPrice = Math.round(CurrentOrderPrice * 100) / 100
   return CurrentOrderPrice
 }
 function DisplayCurrentOrder() {
-  if (document.getElementById("finish order").innerHTML!="Confirm?"){
+  if (document.getElementById("finish order").innerHTML != "Confirm?") {
     let result = CalculatePrice()
-    document.getElementById("WarningMsg").innerHTML= "$"+result+"<br>"+"Confirm?";
-    document.getElementById("finish order").innerHTML="<p><a href='/confirm.html' alt=Confirm?>Confirm?</a></p>"
+    document.getElementById("WarningMsg").innerHTML = "$" + result + "<br>" + "Confirm?";
+    document.getElementById("finish order").innerHTML = "<p><a href='/confirm.html' alt=Confirm?>Confirm?</a></p>"
   }
 }
