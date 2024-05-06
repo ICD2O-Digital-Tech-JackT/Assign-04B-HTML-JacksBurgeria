2433c82a18833c75bdb007557fb89d60ec028886
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-05-06 21:32:12 +0000

    Added final comments

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
+/*Main variables*/
 let Size = document.getElementById("sizeselect");
 let PicklesN = 0;
 let TomatoesN = 0;
 let MeatN = 1;
 let Drink = document.getElementById("drink");
 let CurrentOrderPrice = 0
+/*Tax constant*/
 const HST = 0.13;
-
+/*function to change the order buttons text so that it says finish order, incase the text was changed to something else */
 function ResetText() {
   document.getElementById("finish order").innerHTML = "Finish Order"
 }
-
+/* function to change the visuals of the site (ex. toppings num progress bad etc.)*/
 function UpdateVisuals() {
   ResetText()
+  /*Updating variables*/
   Drink = document.getElementById("drink");
   Size = document.getElementById("sizeselect").value;
+  /*Making toppings display when they are updated*/
   document.getElementById('pickleDis').innerHTML = "Pickles" + "(" + PicklesN + ")";
   document.getElementById('tomatoDis').innerHTML = "Tomatoes" + "(" + TomatoesN + ")";
   document.getElementById('meatDis').innerHTML = "Meat" + "(" + MeatN + ")";
+  /*Updating progress bar colours to show what information has been entered*/
   if (PicklesN != 0 || TomatoesN != 0 || MeatN != 1) {
     document.getElementById("BurgProg").style.backgroundColor = "green";
   } else if (Size != "medium") {
@@ -24,14 +29,16 @@ function UpdateVisuals() {
   } else {
     document.getElementById("BurgProg").style.backgroundColor = "gray";
   }
-  if (Drink.value != "none") {
+  /*Updating drink progress*/
+  if (Drink.value != "notchosen") {
     document.getElementById("DrinkProg").style.backgroundColor = "green";
   } else {
     document.getElementById("DrinkProg").style.backgroundColor = "gray";
   }
 }
-
+/*function to change topping numbers UP one*/
 function addTop(Type) {
+  /*Making sure toppings don't go above 5 before changing their amounts*/
   if (Type == "Pickles") {
     if (PicklesN < 5) {
       PicklesN = PicklesN + 1;
@@ -47,7 +54,9 @@ function addTop(Type) {
   }
   UpdateVisuals()
 }
+/*function to change topping numbers DOWN one*/
 function delTop(Type) {
+  /*Making sure toppings don't go below 0 before changing their amounts*/
   if (Type == "Pickles") {
     if (PicklesN > 0) {
       PicklesN = PicklesN - 1;
@@ -63,11 +72,12 @@ function delTop(Type) {
   }
   UpdateVisuals()
 }
-
+/*function to calculate and return the price of the order*/
 function CalculatePrice() {
   Drink = document.getElementById("drink");
   Size = document.getElementById("sizeselect").value;
   CurrentOrderPrice = 0
+  /*Burger price*/
   if (Size == "medium") {
     CurrentOrderPrice += 2.99
   } else if (Size == "large") {
@@ -75,21 +85,23 @@ function CalculatePrice() {
   } else if (Size == "jumbotron") {
     CurrentOrderPrice += 7.99
   }
+  /*Toppings price*/
   CurrentOrderPrice += (PicklesN * 0.25)
   CurrentOrderPrice += (TomatoesN * 0.25)
   CurrentOrderPrice += ((MeatN - 1) * 0.85)/*Minus one because only extra meat adds to the price*/
-
+  /*Drink price*/
   if (Drink.value != "notchosen") {
     CurrentOrderPrice += 3.50
   }
-  CurrentOrderPrice *= 1.13 /*Tax*/
-  CurrentOrderPrice = Math.round(CurrentOrderPrice * 100) / 100
-  return CurrentOrderPrice
+  CurrentOrderPrice *= 1+HST /*Tax*/
+  CurrentOrderPrice = Math.round(CurrentOrderPrice * 100) / 100  /*Rounding price*/
+  return CurrentOrderPrice /*returning the price of the order*/
 }
+/* function to call the calculate function and display the price of the order*/
 function DisplayCurrentOrder() {
   if (document.getElementById("finish order").innerHTML != "Confirm?") {
-    let result = CalculatePrice()
-    document.getElementById("WarningMsg").innerHTML = "$" + result + "<br>" + "Confirm?";
-    document.getElementById("finish order").innerHTML = "<p><a href='/confirm.html' alt=Confirm?>Confirm?</a></p>"
+    let result = CalculatePrice()/*Calling the function to get the price*/
+    document.getElementById("WarningMsg").innerHTML = "$" + result + "<br>" + "Confirm?";/*Setting the text to ask the user if they want to confirm the order*/
+    document.getElementById("finish order").innerHTML = "<p><a href='/confirm.html' alt=Confirm?>Confirm?</a></p>" /*Setting the button to a link so that when the user presses the order button one more time they get sent to the confirmed order page*/
   }
 }
